a6cdcbfe6c82da1b05548be5881fbdcc84e4f331
José Rodríguez Rico <90771999+uo276922@users.noreply.github.com>
2022-11-17 11:27:42 +0100

    Delete InfoNavegador.js

--- InfoNavegador.js
+++
@@ -1,8 +0,0 @@
-var navegador = new Object();
-navegador.nombre = navigator.appName;
-navegador.idioma = navigator.language;
-navegador.version = navigator.appVersion;
-navegador.plataforma = navigator.platform;
-navegador.vendedor = navigator.vendor;
-navegador.agente = navigator.userAgent;
-navegador.javaActivo = navigator.javaEnabled();
