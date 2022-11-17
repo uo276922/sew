7a4f8d36b10f12b707c304a47d203ecf9448d71f
José Rodríguez Rico <90771999+uo276922@users.noreply.github.com>
2022-11-17 11:16:47 +0100

    Add files via upload

---
+++ IdiomaNavegador.js
@@ -0,0 +1,1 @@
+document.write("Idioma: "+ navegador.idioma);
---
+++ MasInfoNavegador.js
@@ -0,0 +1,5 @@
+document.write("Versión: "+navegador.version);
+document.write("Plataforma: "+navegador.plataforma);
+document.write("Vendedor: "+navegador.vendedor);
+document.write("Agente: "+navegador.agente);
+document.write("Java Activo: "+navegador.javaActivo);
