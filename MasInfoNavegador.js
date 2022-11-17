aa16264ca0ba663b792463d3f9814139f69c85d5
José Rodríguez Rico <90771999+uo276922@users.noreply.github.com>
2022-11-17 11:27:48 +0100

    Delete MasInfoNavegador.js

--- MasInfoNavegador.js
+++
@@ -1,5 +0,0 @@
-document.write("Versión: "+navegador.version);
-document.write("Plataforma: "+navegador.plataforma);
-document.write("Vendedor: "+navegador.vendedor);
-document.write("Agente: "+navegador.agente);
-document.write("Java Activo: "+navegador.javaActivo);
