2466b2fc5ee6f4c2925a4bba02da6ae20a7306a7
José Rodríguez Rico <90771999+uo276922@users.noreply.github.com>
2022-12-04 11:52:19 +0100

    Add files via upload

---
+++ Ejercicio11.js
@@ -0,0 +1,42 @@
+"use strict";
+class GeoGogleMaps {
+    constructor (){
+        
+    }
+    initMapDinamico(){
+        var centro = {lat: 43.3672702, lng: -5.8502461};
+        var mapaGeoposicionado = new google.maps.Map(document.getElementsByTagName('main')[0],{
+            zoom: 8,
+            center:centro,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        
+        var infoWindow = new google.maps.InfoWindow;
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                    var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Localización encontrada');
+                infoWindow.open(mapaGeoposicionado);
+                mapaGeoposicionado.setCenter(pos);
+            }, function() {
+                handleLocationError(true, infoWindow, mapaGeoposicionado.getCenter());
+            });
+        } else {
+        handleLocationError(false, infoWindow, mapaGeoposicionado.getCenter());
+        }
+    }
+
+    handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: Ha fallado la geolocalización' :
+                              'Error: Su navegador no soporta geolocalización');
+        infoWindow.open(mapaGeoposicionado);
+    }
+}
+var mapaDinamicoGoogle = new GeoGogleMaps();
